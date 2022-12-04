5558d95f766e7551a204533226a622bfe92e0be7
dvlprJobayer <nesarahammed019@gmail.com>
2022-12-04 09:58:40 +0600

    initial commit

---
+++ app/user/about/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Hello about from app folder</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ pages/contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Hello contact page from page folder</h1>
+    </div>
+  );
+};
+
+export default Contact;
